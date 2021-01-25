345756bd897a36ca9434c9a215ee04dc355915fc
Borrabeer <borrabeer@outlook.com>
2021-01-25 15:54:16 +0700

    update settings.py

--- CTHS/settings.py
+++ CTHS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r6-(xb_2@la%j!bn+v%xo@^j*j=-ffl1q3*0xe+g&hojbeh$zq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'cths-app.herokuapp.com']
 
 
 # Application definition
